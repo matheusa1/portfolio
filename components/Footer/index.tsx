5d195333600a653e2e9686b7f727708658d436c1
Matheus Andrade <2002matheus@gmail.com>
2023-02-17 08:31:06 -0300

    add github button

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -10,17 +10,17 @@ export const Footer = (): ReactElement => {
 				<Link
 					href={'https://www.linkedin.com/in/matheus-andrade-a8a6b6233/'}
 					target='_blank'
-					className='opacity-50 transition hover:scale-110 hover:opacity-100'
+					className='opacity-50 transition hover:scale-110 hover:opacity-100 hover:text-bluePrimary'
 				>
 					LinkedIn
 				</Link>
-				{/* <Link
-					href={''}
+				<Link
+					href={'https://github.com/matheusa1'}
 					target='_blank'
-					className='opacity-50 transition hover:scale-110 hover:opacity-100'
+					className='opacity-50 transition hover:scale-110 hover:opacity-100 hover:text-bluePrimary'
 				>
-					Instagram
-				</Link> */}
+					Github
+				</Link>
 			</div>
 			<span className='opacity-80'>@{date.getFullYear()}</span>
 		</div>
