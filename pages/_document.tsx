dfe41055a0bebabdd3ab460286f49821c08b1482
Matheus Andrade <2002matheus@gmail.com>
2023-04-11 14:41:09 -0300

    b

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,12 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
 	return (
 		<Html lang='en'>
-			<Head />
+			<Head>
+				<link
+					rel='icon'
+					href={'/favicon.ico'}
+				/>
+			</Head>
 			<body className='scrollbar bg-bgColor'>
 				<Main />
 				<NextScript />
