b14bd24639198fbff437887968cb949dd527fd90
Matheus Andrade <2002matheus@gmail.com>
2023-04-13 21:17:22 -0300

    a

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,53 +1,25 @@
-import React from 'react'
-import * as glob from 'glob'
-
 const Sitemap = () => {
 	return null
 }
 
 export const getServerSideProps = async ({ res }: any) => {
-	const BASE_URL = 'https://matheussandrade.com.br'
-
-	const pagesDir = 'pages/**/*.tsx'
-	let pagesPaths = await glob.sync(pagesDir)
-
-	pagesPaths = pagesPaths
-		.filter((path) => !path.includes('['))
-		.filter((path) => !path.includes('/_'))
-		.filter((path) => !path.includes('404'))
-
-	const staticPaths = pagesPaths
-		.filter((staticPage) => {
-			return ![
-				'_app.tsx',
-				'_document.tsx',
-				'404.tsx',
-				'sitemap.xml.tsx',
-			].includes(staticPage)
-		})
-		.map((staticPagePath) => {
-			staticPagePath = staticPagePath.replace('pages/', '')
-			staticPagePath = staticPagePath.replace('/index.tsx', '')
-
-			return `${BASE_URL}/${staticPagePath}`
-		})
-
-	const allPaths = [...staticPaths]
-
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${allPaths
-				.map((url) => {
-					return `
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `
-				})
-				.join('')}
+		<url>
+<loc>https://www.matheussandrade.com.br</loc>
+</url>
+<url>
+<loc>https://www.matheussandrade.com.br/contact</loc>
+</url>
+<url>
+<loc>https://www.matheussandrade.com.br/portfolio</loc>
+</url>
+<url>
+<loc>https://www.matheussandrade.com.br/frontend</loc>
+</url>
+<url>
+<loc>https://www.matheussandrade.com.br/</loc>
+</url>
     </urlset>
   `
 
